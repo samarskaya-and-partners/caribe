b67ec82c13794cbf56d0faaa45c287d29836010a
Josh Reisner <josh@joshreisner.com>
2014-09-18 14:48:58 -0700

    fixing gulp issues, removing bower components

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,41 @@ var gutil 		= require('gulp-util');
 var notify 		= require('gulp-notify');
 var sass 		= require('gulp-ruby-sass');
 var autoprefix 	= require('gulp-autoprefixer');
-var sassDir		= 'sass';
-var cssDir		= 'public/assets/css';
+var minifyCSS 	= require('gulp-minify-css');
+var rename		= require('gulp-rename');
+var include		= require('gulp-include');
+var uglify		= require('gulp-uglify');
+
+var sassDir		= 'assets/sass';
+var jsDir		= 'assets/js';
+var outputDir	= 'public/assets';
 
 gulp.task('main-css', function(){
 	return gulp.src(sassDir + '/main.sass')
-		.pipe(sass({
-			style: 'compressed',
-	        onError: function(err) {
-	            return notify(err);
-	        }
-		}))
+		.pipe(sass())
+		.on('error', handleError)
 		.pipe(autoprefix('last 3 version'))
-		.pipe(gulp.dest(cssDir))
-		.pipe(notify('css compiled'));
+		.pipe(minifyCSS({keepSpecialComments:0}))
+        .pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest(outputDir + '/css'));
+});
+
+gulp.task('main-js', function(){
+	return gulp.src(jsDir + '/main.js')
+		.pipe(include())
+		.pipe(uglify({mangle:false}))
+        .pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest(outputDir + '/js'));
 });
 
 gulp.task('watch', function(){
 	gulp.watch(sassDir + '/**/*.sass', ['main-css']);
+	gulp.watch(jsDir + '/**/*.js', ['main-js']);
 });
 
-gulp.task('default', ['main-css', 'watch']);
+gulp.task('default', ['main-css', 'main-js', 'watch']);
+
+function handleError(err) {
+	gulp.src(sassDir + '/main.sass').pipe(notify(err));
+	this.emit('end');
+}
